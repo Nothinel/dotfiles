5dd2a271f6f7e98d8e501f5c6d3b6676e66fb073
Nothinel <konrad-ritter@gmx.de>
2019-03-04 22:17:32 +0100

    Finally got lsd and nerd fonts working, now using Source Code Pro in urxvt

--- getfiles.sh
+++ getfiles.sh
@@ -5,4 +5,4 @@ cp ~/.config/i3status/config ./i3statusArchX230
 cp ~/.vimrc ./vimrcArchX230
 cp ~/.config/vifm/vifmrc ./vifmrcArchX230
 cp ~/.config/vifm/colors/matrix.vifm ./vifmColorSchemeArchX230
-
+cp ~/.Xresources ./XresourcesArchX230
