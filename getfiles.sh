0941d81c333164aa37c9a50f947825096fd677d8
Nothinel <konrad-ritter@gmx.de>
2019-03-05 12:24:19 +0100

    Updated getfiles for bashrc and bash_profile

--- getfiles.sh
+++ getfiles.sh
@@ -6,3 +6,5 @@ cp ~/.vimrc ./vimrcArchX230
 cp ~/.config/vifm/vifmrc ./vifmrcArchX230
 cp ~/.config/vifm/colors/matrix.vifm ./vifmColorSchemeArchX230
 cp ~/.Xresources ./XresourcesArchX230
+cp ~/.bashrc ./bashrcArchX230
+cp ~/.bash_profile ./bash_profileArchX230
