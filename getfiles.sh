03946a93be8a261b376c0a7c88a8586a39d6b7cf
Nothinel <konrad-ritter@gmx.de>
2019-03-07 11:14:07 +0100

    corrected getfiles for sef06 name

--- getfiles.sh
+++ getfiles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp ~/.config/i3/config ./i3ArchX230
-cp ~/.config/i3status/config ./i3statusArchX230
-cp ~/.vimrc ./vimrcArchX230
-cp ~/.config/vifm/vifmrc ./vifmrcArchX230
-cp ~/.config/vifm/colors/matrix.vifm ./vifmColorSchemeArchX230
-cp ~/.Xresources ./XresourcesArchX230
-cp ~/.bashrc ./bashrcArchX230
-cp ~/.bash_profile ./bash_profileArchX230
+cp ~/.config/i3/config ./i3sef06
+cp ~/.config/i3status/config ./i3statussef06
+cp ~/.vimrc ./vimrcsef06
+cp ~/.config/vifm/vifmrc ./vifmrcsef06
+cp ~/.config/vifm/colors/matrix.vifm ./vifmColorSchemesef06
+cp ~/.Xresources ./Xresourcessef06
+cp ~/.bashrc ./bashrcsef06
+cp ~/.bash_profile ./bash_profilesef06
 
