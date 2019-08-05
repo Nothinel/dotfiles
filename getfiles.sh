95b1eb2c777fae4463442b3ea24419e608b63d0c
Nothinel <nothinel@hotmail.de>
2019-08-05 12:40:03 +0200

    Added xinitrc to this branch

--- getfiles.sh
+++ getfiles.sh
@@ -8,4 +8,4 @@ cp ~/.config/vifm/colors/matrix.vifm ./vifmColorSchemesef06
 cp ~/.Xresources ./Xresourcessef06
 cp ~/.bashrc ./bashrcsef06
 cp ~/.bash_profile ./bash_profilesef06
-
+cp ~/.xinitrc ./xinitrcsef06
