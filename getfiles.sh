dbcd02433e34004aad80cfa6f50c88e4fab6da0a
Nothinel <nothinel@hotmail.de>
2020-07-06 05:22:04 +0200

    Create new branch for nitro 5 WIndows Subsystem for Linux

--- getfiles.sh
+++ getfiles.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cp ~/.config/i3/config ./i3sef06
-cp ~/.config/i3status/config ./i3statussef06
-cp ~/.vimrc ./vimrcsef06
-cp ~/.config/vifm/vifmrc ./vifmrcsef06
-cp ~/.config/vifm/colors/matrix.vifm ./vifmColorSchemesef06
-cp ~/.Xresources ./Xresourcessef06
-cp ~/.bashrc ./bashrcsef06
-cp ~/.bash_profile ./bash_profilesef06
-cp ~/.xinitrc ./xinitrcsef06
+cp ~/.vimrc ./vimrc_nitro5
+cp ~/.bashrc ./bashrc_nitro5
+#cp ~/.bash_profile ./bash_profile_nitro5
