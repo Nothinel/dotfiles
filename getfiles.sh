c899850a988837553665abf2d61d7e3a67eeb1ce
Nothinel <nothinel@hotmail.de>
2021-02-17 15:50:11 +0100

    added a bit and improved vimrc

--- getfiles.sh
+++ getfiles.sh
@@ -4,7 +4,7 @@ cp ~/.config/i3/config ./i3sef06
 cp ~/.config/i3status/config ./i3statussef06
 cp ~/.vimrc ./vimrcsef06
 cp ~/.config/vifm/vifmrc ./vifmrcsef06
-cp ~/.config/vifm/colors/matrix.vifm ./vifmColorSchemesef06
+#cp ~/.config/vifm/colors/matrix.vifm ./vifmColorSchemesef06
 cp ~/.Xresources ./Xresourcessef06
 cp ~/.bashrc ./bashrcsef06
 cp ~/.bash_profile ./bash_profilesef06
