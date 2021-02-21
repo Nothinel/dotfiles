305325615077e5d7c2065fa06c374ad10852c0e2
Nothinel <nothinel@hotmail.de>
2021-02-21 17:21:51 +0100

    Restarting the master branch to be like the gold standard and minimum in one

--- getfiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp ~/.config/i3/config ./i3ArchX230
-cp ~/.config/i3status/config ./i3statusArchX230
-cp ~/.vimrc ./vimrcArchX230
-cp ~/.config/vifm/vifmrc ./vifmrcArchX230
-cp ~/.config/vifm/colors/matrix.vifm ./vifmColorSchemeArchX230
-cp ~/.Xresources ./XresourcesArchX230
--- getfiles_LOCAL_4455.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp ~/.config/i3/config ./i3sef06
-cp ~/.config/i3status/config ./i3statussef06
-cp ~/.vimrc ./vimrcsef06
-cp ~/.config/vifm/vifmrc ./vifmrcsef06
-cp ~/.config/vifm/colors/matrix.vifm ./vifmColorSchemesef06
-
