ac6bd96802ebafb7db6422cd37d6e4deba25c64c
Nothinel <nothinel@hotmail.de>
2025-08-05 13:29:46 +0200

    updates

--- getfiles.sh
+++ getfiles.sh
@@ -3,10 +3,10 @@
 cp ~/.config/i3/config ./i3sef06
 cp ~/.config/i3status/config ./i3statussef06
 cp ~/.vimrc ./vimrcsef06
-cp ~/.config/vifm/vifmrc ./vifmrcsef06
+#cp ~/.config/vifm/vifmrc ./vifmrcsef06
 #cp ~/.config/vifm/colors/matrix.vifm ./vifmColorSchemesef06
 cp ~/.Xresources ./Xresourcessef06
 cp ~/.bashrc ./bashrcsef06
 cp ~/.bash_profile ./bash_profilesef06
 cp ~/.xinitrc ./xinitrcsef06
-cp ~/.config/dunst/dunstrc ./dunstrc
+# cp ~/.config/dunst/dunstrc ./dunstrc
