b401952e664c734836e0a3710ab4bad8892a1b44
Nothinel <nothinel@hotmail.de>
2025-09-29 15:24:14 +0200

    switched to lazyvim as a startingpoint for own config

--- nvim/lua/plugins/onedark.lua
+++ nvim/lua/plugins/onedark.lua
@@ -1,12 +1,21 @@
--- Using Lazy
-return{
-  "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('onedark').setup {
-      style = 'darker'
-    }
-    -- Enable theme
-    require('onedark').load()
-  end
+return {
+  -- add gruvbox
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+      })
+      -- Enable theme
+      require("onedark").load()
+    end,
+  },
+  -- Configure LazyVim to load onedark
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
 }
--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,11 +1,16 @@
-return{{
-  "lervag/vimtex",
-  lazy = false,     -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
-  init = function()
-    -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "general"
-    vim.g.vimtex_view_general_viewer = "evince"
-    vim.g.vimtex_compiler_method = "pdflatex"
-  end
-}}
+return {
+  {
+    "lervag/vimtex",
+    lazy = false, -- lazy-loading will disable inverse search
+    config = function()
+      vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+      vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+      vim.g.vimtex_view_method = "general"
+      vim.g.vimtex_view_general_viewer = "evince"
+      vim.g.vimtex_compiler_method = "pdflatex"
+    end,
+    keys = {
+      { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
+    },
+  },
+}
