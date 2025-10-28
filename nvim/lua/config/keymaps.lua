bc64681002b109685afd8173d10f8d57e489fe48
Nothinel <konrad-ritter@posteo.de>
2025-10-28 13:47:16 +0100

    added tweaks and keymaps from sef06 branch

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -43,10 +43,37 @@ vim.keymap.set(
   ":%s/INCLUDESOLUTIONStrue/INCLUDESOLUTIONSfalse/<Enter>:update<Enter><C-o>",
   { desc = "EP: exclude solutions" }
 )
---":%s/<Bslash>(<Bslash><Bslash>SERIE[IVX]<Bslash>+<Bslash>)true/<Bslash>1false/<Enter>:%s/<Bslash>(<Bslash><Bslash>SERIEI<Bslash>)false/<Bslash>1true/<Enter>:update<Enter><C-o>",
 vim.keymap.set("n", "<localleader>ss", function()
   local expr = vim.fn.input("roman number of series?: ")
+  local cursorpos = vim.api.nvim_win_get_cursor(0)
   vim.cmd([[%s/\(\SERIE[IVX]\+\)true/\1false/]])
   vim.cmd(string.format("%%s/\\vSERIE%sfalse/SERIE%strue/", expr, expr))
+  vim.api.nvim_win_set_cursor(0, cursorpos)
   vim.cmd("update")
 end, { desc = "EP: select series" })
+vim.keymap.set("n", "<localleader>so", function()
+  local expr = vim.fn.input("which task number?")
+  --vim.cmd(string.format("/task{[MZAWLE]}{%s}", expr))
+  local line = vim.fn.search(string.format("task{[MZAWLE]}{%s}", expr))
+  if line == 0 then
+    print("did not find task")
+    return
+  end
+  local linetext = vim.fn.getline(line)
+  local path, filename =
+    linetext:match(string.format("\\task{[MZAWLE]}{%s}{.-}{%%s*([^}]+)%%s*}{%%s*([^}]+)%%s*}", expr))
+  if not path or not filename then
+    print("unable to extract filename or path from line")
+    return
+  end
+  local fullpath = string.format("%s/%s.tex", path, filename)
+  vim.cmd("edit " .. fullpath)
+end, { desc = "EP: open task", silent = true })
+-- Keymap: run latexindent on the current file
+vim.keymap.set("n", "<localleader>lf", function()
+  local filename = vim.fn.expand("%:p")
+  vim.cmd("silent! write") -- save first
+  vim.fn.system({ "latexindent", "-m", "-w", "-c=/home/kr80cora/NextCloud/Dokumente/latex_backups", filename })
+  vim.cmd("edit!") -- reload the buffer
+  print("Formatted with latexindent")
+end, { desc = "Format with latexindent" })
